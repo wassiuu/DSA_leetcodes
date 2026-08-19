40c4593ddb595c2bcc65f947ed8913934ba64362
Wasi <wasiabbas206@gmail.com>
2026-08-18 23:29:19 -0600

    lc 205

---
+++ hashing/205_isomorphic_strings.py
@@ -0,0 +1,39 @@
+'''
+Given two strings s and t, determine if they are isomorphic.
+
+Two strings s and t are isomorphic if the characters in s 
+can be replaced to get t.
+
+All occurrences of a character must be replaced with another 
+character while preserving the order of characters. No two 
+characters may map to the same character, but a character may 
+map to itself.
+
+
+Example 1:
+Input: s = "egg", t = "add"
+Output: true
+Explanation:
+The strings s and t can be made identical by:
+Mapping 'e' to 'a'.
+Mapping 'g' to 'd'.
+
+Example 2:
+Input: s = "f11", t = "b23"
+Output: false
+Explanation:
+The strings s and t can not be made identical as '1' needs to be mapped to both '2' and '3'.
+
+Example 3:
+Input: s = "paper", t = "title"
+Output: true
+'''
+
+def isIsomorphic(s, t):
+        map = {}
+        for i in range(len(s)):
+            if (s[i] in map and t[i] != map[s[i]]) or (s[i] not in map.keys() and t[i] in map.values()) :
+                return False
+            else:
+                map[s[i]] = t[i]
+        return True
