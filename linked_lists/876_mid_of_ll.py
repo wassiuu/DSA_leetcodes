bd1ded404710d0da7dc990a1c139e0b296dda631
Wasi <wasiabbas206@gmail.com>
2026-08-25 21:26:02 -0600

    lc 876 LL

--- linked_lists/876_mid_of_ll.py
+++ linked_lists/876_mid_of_ll.py
@@ -1,5 +1,21 @@
 '''
-Given the head of a singly linked list, return the middle node of the linked list.
+Given the head of a singly linked list, return the middle
+ node of the linked list.
 
 If there are two middle nodes, return the second middle node.
 '''
+
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution(object):
+    def middleNode(self, head):
+        slow = head
+        fast = head
+
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+        return slow
