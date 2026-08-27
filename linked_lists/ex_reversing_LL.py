0e5319118abf1234298aba7773a3588299e73c58
Wasi <wasiabbas206@gmail.com>
2026-08-26 21:43:27 -0600

    files created

--- linked_lists/ex_reversing_LL.py
+++ linked_lists/ex_reversing_LL.py
@@ -8,10 +8,10 @@ themselves may be changed.)
 '''
 
 # Definition for singly-linked list.
-# class ListNode(object):
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
+class ListNode(object):
+     def __init__(self, val=0, next=None):
+         self.val = val
+         self.next = next
 class Solution(object):
     def swapPairs(self, head):
         dummy = ListNode(0)
