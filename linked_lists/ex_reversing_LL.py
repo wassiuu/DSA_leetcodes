dc16c15c4531509d6c8f7167085fcd69f3d3e84d
Wasi <wasiabbas206@gmail.com>
2026-08-26 20:53:19 -0600

    lc 24 done LL

--- linked_lists/ex_reversing_LL.py
+++ linked_lists/ex_reversing_LL.py
@@ -7,3 +7,26 @@ modifying the values in the list's nodes (i.e., only nodes
 themselves may be changed.)
 '''
 
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution(object):
+    def swapPairs(self, head):
+        dummy = ListNode(0)
+        dummy.next = head 
+        current = dummy
+
+        while current.next and current.next.next:
+            first = current.next
+            second = current.next.next 
+
+            first.next = second.next 
+            second.next = first
+            current.next = second 
+
+            current = current.next.next 
+        
+        return dummy.next
+
