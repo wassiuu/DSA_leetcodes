2eabdc68e263db70a08bf043ed94d6bf470af0f3
Wasi <wasiabbas206@gmail.com>
2026-08-30 17:42:04 -0600

    added code for lc 82

--- linked_lists/82_rv_dupes_sortedLL.py
+++ linked_lists/82_rv_dupes_sortedLL.py
@@ -12,3 +12,32 @@ Input: head = [1,1,1,2,3]
 Output: [2,3]
 '''
 
+# Definition for singly-linked list.
+class ListNode(object):
+     def __init__(self, val=0, next=None):
+         self.val = val
+         self.next = next
+class Solution(object):
+    def deleteDuplicates(self, head):
+        dummy = ListNode(0)
+        dummy.next = head
+
+        prev = dummy
+        curr = head 
+        
+        while curr and curr.next:
+            
+            if curr.val == curr.next.val:
+                #make the curr move to the last dupe (skip all the dupes)
+                while curr.next and curr.val == curr.next.val:
+                    curr = curr.next
+
+                #make prev pointer point to the element after the last dupe
+                prev.next = curr.next
+            else:
+                prev = prev.next
+            
+            curr = curr.next
+
+        
+        return dummy.next
