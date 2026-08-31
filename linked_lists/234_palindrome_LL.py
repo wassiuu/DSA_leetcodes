5fe326e7af0dd1a411f15f779a88c57c07364701
Wasi <wasiabbas206@gmail.com>
2026-08-30 18:49:06 -0600

    completed lc 234

--- linked_lists/234_palindrome_LL.py
+++ linked_lists/234_palindrome_LL.py
@@ -11,5 +11,32 @@ Input: head = [1,2]
 Output: false
 '''
 
-
+# Definition for singly-linked list.
+class ListNode(object):
+     def __init__(self, val=0, next=None):
+         self.val = val
+         self.next = next
+class Solution(object):
+    def isPalindrome(self, head):
+        slow = head
+        fast = head
+        while fast and fast.next:
+            fast = fast.next.next
+            slow = slow.next 
+        
+        curr = slow
+        prev = None 
+        while curr:
+            next_node = curr.next
+            curr.next = prev 
+            prev = curr
+            curr = next_node 
+        
+        left, right = head, prev
+        while right:
+            if left.val != right.val:
+                return False
+            left=left.next
+            right = right.next
+        return True
     
