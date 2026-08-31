8b949d9a2d56004dd1047b81240db630002b6850
Wasi <wasiabbas206@gmail.com>
2026-08-30 18:06:29 -0600

    completed lc 1721

--- linked_lists/1721_swap_nodes_in_LL.py
+++ linked_lists/1721_swap_nodes_in_LL.py
@@ -13,3 +13,31 @@ Example 2:
 Input: head = [7,9,6,6,7,8,3,0,9,5], k = 5
 Output: [7,9,6,6,8,7,3,0,9,5]
 '''
+
+# Definition for singly-linked list.
+class ListNode(object):
+     def __init__(self, val=0, next=None):
+         self.val = val
+         self.next = next
+class Solution(object):
+    def swapNodes(self, head, k):
+        dummy = ListNode(0)
+        dummy.next = head
+
+        first_p = dummy
+        second_p = dummy
+        val1 = dummy
+
+        for _ in range(k):
+            second_p = second_p.next
+        val1 = second_p
+
+        while second_p and second_p.next:
+            first_p = first_p.next
+            second_p = second_p.next
+        
+        temp = val1.val
+        val1.val = first_p.next.val
+        first_p.next.val = temp
+
+        return dummy.next
