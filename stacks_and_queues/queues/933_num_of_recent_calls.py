c97721b69b437b6ac38a7d2e3492963355e668cb
Wasi <wasiabbas206@gmail.com>
2026-09-13 23:26:26 -0600

    made changes

--- stacks_and_queues/queues/933_num_of_recent_calls.py
+++ stacks_and_queues/queues/933_num_of_recent_calls.py
@@ -32,3 +32,7 @@ recentCounter.ping(3001);  // requests = [1, 100, 3001], range is [1,3001], retu
 recentCounter.ping(3002);  // requests = [1, 100, 3001, 3002], range is [2,3002], return 3
  
 '''
+
+'''
+really busy today couldnt do it
+'''
